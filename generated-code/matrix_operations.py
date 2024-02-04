b3c3ba67852ddd3318572cc80848ec4ace0f0fa9
biigdata <sokomori@yahoo.fr>
2024-02-04 13:22:00 +0100

    Fixed initialisation issue in multiply_matrices

--- generated-code/matrix_operations.py
+++ generated-code/matrix_operations.py
@@ -4,9 +4,12 @@
 def multiply_matrices(mat1, mat2):
     result = []
     for i in range(len(mat1)):
+        row = []
         for j in range(len(mat2[0])):
-            result[i][j] = sum(mat1[i][k] * mat2[k][j] for k in range(len(mat2)))
+            row.append(sum(mat1[i][k] * mat2[k][j] for k in range(len(mat2))))
+        result.append(row)
     return result
+
 
 matrix1 = [[1, 2], [3, 4]]
 matrix2 = [[5, 6], [7, 8]]
