8f2a4648f5bae98780cc6c940a485bb0cee4f566
D evilll <157961276+wissalyahia@users.noreply.github.com>
2024-02-05 16:25:28 +0100

    Correct code

---
+++ wissal+test/lglsi2-24/generated-code/test.py
@@ -0,0 +1,9 @@
+def calcule_somme(n):
+    somme = 0
+    for i in range(1, n+1)
+        somme += i
+    return somme
+
+nombre = 5
+resultat = calcule_somme(nombre)
+print(f"La somme des entiers de 1 à {nombre} est : {resultat}")
