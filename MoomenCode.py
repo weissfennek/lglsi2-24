4927fcc16d0ea8f4507068bc5900e793c7c711d1
RIM <rimmoussaoui21@gmai.com>
2024-02-12 22:15:35 +0100

    test of tiangle code

--- MoomenCode.py
+++ MoomenCode.py
@@ -13,6 +13,7 @@ def is_triangle(side1, side2, side3):
 
     if side1 >= side2 - side3 or side2 >= side1 + side3 or side3 > side1 + side2:
         return True
+    # reformuler cette fonction
 
     return False
 
