9c141f07bc64823abaa8fa6fc4572d0ef4b34659
Younoussa Hassani <157608091+nassim-ali@users.noreply.github.com>
2024-02-12 19:52:02 +0100

    Update square_sum.py bugs fixing

--- generated-code/square_sum.py
+++ generated-code/square_sum.py
@@ -5,10 +5,10 @@ def calculate_square_sum(numbers: list) -> int | float:
     Calculate the sum of squares for a given list of numbers.
     """
     squared_numbers = [num ** 2 for num in numbers]
-    return sum(numbers)
+    return sum(squared_numbers)
 
 # List of integer
-mixed_numbers = [1, 2, "3", 4, 5]
+mixed_numbers = [1, 2, 3, 4, 5]
 
 # Function call with the mixed_numbers list
 result = calculate_square_sum(mixed_numbers)
