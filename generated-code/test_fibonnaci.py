a80e6989e6e08c0e2aabac3f55c78779c39d1fb6
roua04 <158773877+roua04@users.noreply.github.com>
2024-02-14 00:42:00 +0100

    added tests that cover the fibonaccipy.closes#107

---
+++ generated-code/test_fibonnaci.py
@@ -0,0 +1,13 @@
+
+from fibonacci import fibonacci
+
+def test_fibonacci():
+    assert fibonacci(0) == 0
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+
